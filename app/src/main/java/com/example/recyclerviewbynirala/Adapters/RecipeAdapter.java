b14c36155d022356214f0b18c3f06c42120e5c4a
Nitish Kumar <niralanitrogensir@gmail.com>
2021-01-18 22:09:03 +0530

    This is fourth commit. code is working properly. and showing all recycler view images and text.

--- app/src/main/java/com/example/recyclerviewbynirala/Adapters/RecipeAdapter.java
+++ app/src/main/java/com/example/recyclerviewbynirala/Adapters/RecipeAdapter.java
@@ -15,7 +15,43 @@ import com.example.recyclerviewbynirala.R;
 
 import java.util.ArrayList;
 
-public class RecipeAdapter{
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.viewHolder>{
+    ArrayList<RecipeModel> list;
+    Context context;
+    public RecipeAdapter(ArrayList<RecipeModel> list, Context context) {
+        this.list = list;
+        this.context = context;
+    }
 
 
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(context).inflate(R.layout.sample_recyclerview,parent,false);
+        return new viewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        RecipeModel model =list.get(position);
+        holder.imageView.setImageResource(model.getPicture());
+        holder.textView.setText(model.getText());
+        
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder {
+   ImageView imageView;
+   TextView textView;
+
+    public viewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = itemView.findViewById(R.id.imageView);
+        textView = itemView.findViewById(R.id.textView2);
+    }
+}
 }
